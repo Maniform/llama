adcd583d428e7966d6eb7c9b003e9b0363eb5c33
Maniform <n.villette60@yahoo.fr>
2023-07-24 16:37:08 -0400

    Ajout de la possibilité de définir le prompt system

--- ui_chat_assistant.py
+++ ui_chat_assistant.py
@@ -14,22 +14,52 @@ import threading
 def on_enter(event, send_button):
     send_button.invoke()
 
-def process_send_message(input_entry, text_area, to_disable, dialogs, generator, max_gen_len, temperature, top_p, thread, log_file = None):
+def process_send_message(
+    input_entry,
+    text_area,
+    to_disable,
+    role,
+    dialogs,
+    generator,
+    max_gen_len,
+    temperature,
+    top_p,
+    thread,
+    log_file = None
+):
     question = input_entry.get()
     if question:
-        pos = text_area.index(tk.INSERT)
-        text_area_insert(text_area, pos, "You : " + question + "\n\nAssistant :")
         input_entry.delete(0, 'end')
-        for widget in to_disable:
-            widget.configure(state="disabled")
-        dialogs[0].append({"role": "user", "content": question})
-        if log_file is not None:
-            with open(log_file, 'a') as f:
-                f.write("You : " + question + "\n\nAssistant :")
-        thread = threading.Thread(target=send_message, args=(input_entry, text_area, to_disable, dialogs, generator, max_gen_len, temperature, top_p, log_file))
-        thread.start()
+        if role == "user":
+            dialogs[0].append({"role": role, "content": question})
+            pos = text_area.index(tk.INSERT)
+            text_area_insert(text_area, pos, "You : " + question + "\n\nAssistant :")
+            for widget in to_disable:
+                widget.configure(state="disabled")
+            if log_file is not None:
+                with open(log_file, 'a') as f:
+                    f.write("You : " + question + "\n\nAssistant :")
+            thread = threading.Thread(target=send_message, args=(input_entry, text_area, to_disable, dialogs, generator, max_gen_len, temperature, top_p, log_file))
+            thread.start()
+        elif role == "system":
+            clear_text_area(text_area, dialogs)
+            dialogs.clear()
+            dialogs.extend([[{"role": role, "content": question}]])
+            if log_file is not None:
+                with open(log_file, 'a') as f:
+                    f.write("System : " + question + "\n\n")
 
-def send_message(input_entry, text_area, to_enable, dialogs, generator, max_gen_len, temperature, top_p, log_file = None):
+def send_message(
+    input_entry,
+    text_area,
+    to_enable,
+    dialogs,
+    generator,
+    max_gen_len,
+    temperature,
+    top_p,
+    log_file = None
+):
     pos = text_area.index(tk.INSERT)
     results = generator.chat_completion(
         dialogs,  # type: ignore
@@ -59,7 +89,7 @@ def text_area_insert(text_area, pos, text):
     text_area.see(tk.END)
 
 def clear_text_area(text_area, dialogs):
-    if dialogs[0][0]['role'] == "system":
+    if len(dialogs) > 0 and len(dialogs[0]) > 0 and len(dialogs[0][0]) > 0 and dialogs[0][0]['role'] == "system":
         system_prompt = dialogs[0][0]['content']
         dialogs.clear()
         dialogs.extend([[{"role": "system", "content": system_prompt}]])
@@ -79,7 +109,7 @@ def main(
     max_batch_size: int = 4,
     max_gen_len: Optional[int] = None,
 ):
-    generator = None
+    # generator = None
     generator = Llama.build(
         ckpt_dir=ckpt_dir,
         tokenizer_path=tokenizer_path,
@@ -96,25 +126,31 @@ def main(
     root.title("Assistant Llama")
 
     text_area = scrolledtext.ScrolledText(root, wrap='word', state='disabled')
-    text_area.grid(row=0, column=0, columnspan=3, padx=10, pady=10, sticky='nsew')
+    text_area.grid(row=0, column=0, columnspan=4, padx=10, pady=10, sticky='nsew')
+
+    role_value = tk.StringVar(root)
+    role_value.set("user")
+    options = ["user", "system"]
+    role_menu = tk.OptionMenu(root, role_value, *options)
+    role_menu.grid(row=1, column=0, padx=10, pady=10, sticky='w')
 
     # Créer la ligne d'entrée et le bouton "Envoyer"
     input_entry = tk.Entry(root)
-    input_entry.grid(row=1, column=0, padx=10, pady=10, sticky='ew')
+    input_entry.grid(row=1, column=1, padx=10, pady=10, sticky="ew")
     input_entry.bind('<Return>', lambda event: on_enter(event, send_button))
     input_entry.bind('<KP_Enter>', lambda event: on_enter(event, send_button))
 
-    send_button = tk.Button(root, text="Envoyer", command=lambda: process_send_message(input_entry, text_area, [input_entry, send_button, clear_button], dialogs, generator, max_gen_len, temperature, top_p, thread, "log.txt"))
-    send_button.grid(row=1, column=1, padx=10, pady=10, sticky='e')
+    send_button = tk.Button(root, text="Envoyer", command=lambda: process_send_message(input_entry, text_area, [input_entry, send_button, clear_button], role_value.get(), dialogs, generator, max_gen_len, temperature, top_p, thread, "log.txt"))
+    send_button.grid(row=1, column=2, padx=10, pady=10, sticky='e')
 
     clear_button = tk.Button(root, text="Effacer", command=lambda: clear_text_area(text_area, dialogs))
-    clear_button.grid(row=1, column=2, padx=10, pady=10, sticky='e')
+    clear_button.grid(row=1, column=3, padx=10, pady=10, sticky='e')
 
     input_entry.focus_set()
 
     # Redimensionner les cellules de la grille pour que la zone de texte ait la priorité
     root.grid_rowconfigure(0, weight=1)
-    root.grid_columnconfigure(0, weight=1)
+    root.grid_columnconfigure(1, weight=1)
 
     root.mainloop()
 
